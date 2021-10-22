aa408f34cc42658965d1cf5d1dc2cbe1fdb8ac85
Ken Ilo <keneilo91@yahoo.com>
2021-10-21 22:21:28 -0700

    query the database to test the connection

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+// inquire express
 const express = require('express');
+
+// import mysql2
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +10,24 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Code to connect the application to MYSQL database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'KeNe19(!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// query the database to test the connection
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 // Default response for any other request (Not Found) or
